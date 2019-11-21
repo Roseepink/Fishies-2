6c30476acfc7ad705f5ca4b26d09675ad5262216
Rose <SK2060@live.mdx.ac.uk>
2019-11-21 14:49:21 +0000

    Macbook edit

---
+++ Rhythm Gme/Assets/scripts/CheckForFish.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckForFish : MonoBehaviour
+{
+    private GameObject bButton, rButton, yButton, gButton;
+    private bool inBlue, inRed, inGreed, inYellow;
+    
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "ActiveBlue")
+        {
+            print("Blue object has arrow!!!");
+            inBlue = true;
+            bButton = other.gameObject;
+        }
+        if (other.tag == "ActiveGreen")
+        {
+            inGreed = true;
+            gButton = other.gameObject;
+        }
+        if (other.tag == "ActiveRed")
+        {
+            inRed = true;
+            rButton = other.gameObject;
+        }
+        if (other.tag == "ActiveYellow")
+        {
+            inYellow = true;
+            yButton = other.gameObject;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "ActiveBlue")
+        {
+            inBlue = false;
+        }
+        if (other.tag == "ActiveGreen")
+        {
+            inGreed = false;
+        }
+        if (other.tag == "ActiveRed")
+        {
+            inRed = false;
+        }
+        if (other.tag == "ActiveYellow")
+        {
+            inYellow = false;
+        }
+    }
+
+    public void RedButtonPressed()
+    {
+        if (inRed)
+        {
+            inRed = false;
+            rButton.SetActive(false);
+        }
+    }
+    public void GreedButtonPressed()
+    {
+        if (inGreed)
+        {
+            inGreed = false;
+            gButton.SetActive(false);
+        }
+    }
+    public void BlueButtonPressed()
+    {
+        if (inBlue)
+        {
+            inBlue = false;
+            bButton.SetActive(false);
+        }
+    }
+    public void YellowButtonPressed()
+    {
+        if (inYellow)
+        {
+            inYellow = false;
+            yButton.SetActive(false);
+        }
+    }
+
+}
---
+++ Rhythm Gme/Assets/scripts/Menu_GUI.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Menu_GUI : MonoBehaviour
+
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(1); //Put your scene number in the brackets here. You can find your scene numbers in Build Settings
+    }
+
+    public void QuitGame() //Quits the application.
+    {
+        Debug.Log("Quit!");
+        Application.Quit();
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(0);
+
+    }
+}
